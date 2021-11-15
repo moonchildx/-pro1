2ead4e79723848bec672e994bddbca8dc78805a6
Aina <93551599+moonchildx@users.noreply.github.com>
2021-11-15 13:19:26 +0100

    Create P99182.cc

---
+++ P0 - First contact/P99182.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num1, num2;
+    double meitat;
+    
+    cin >> num1 >> num2;
+    
+    meitat = num1 + num2;
+    
+    cout << meitat/2 << endl;
+
+}
